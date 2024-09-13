8551529413e68d0c9d360b1cc5f3e9fa860e122e
Nn-Exe <nonthaphatkkz@gmail.com>
2024-09-13 12:17:40 +0700

    update product api Render

--- ecommerce-frontend/src/App.js
+++ ecommerce-frontend/src/App.js
@@ -68,7 +68,7 @@ function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:5000/api/products')
+    fetch('https://chunar-ecommerce.onrender.com/api/products')
       .then((response) => response.json())
       .then((data) => setProducts(data))
       .catch((error) => console.error('Error fetching products:', error));
