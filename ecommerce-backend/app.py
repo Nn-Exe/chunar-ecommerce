28e07402d636a561a85c45abda79f09cbcd5fba6
Nn-Exe <nonthaphatkkz@gmail.com>
2024-09-13 13:03:11 +0700

    port 10000

--- ecommerce-backend/app.py
+++ ecommerce-backend/app.py
@@ -16,5 +16,5 @@ def get_products():
     return jsonify(products)
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
